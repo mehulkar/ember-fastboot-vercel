1a73c60de2e1eeaadf81651df85e5cb731af76c2
Mehul Kar <mehul.kar@vercel.com>
2022-08-05 18:11:53 -0700

    Copy config.json into output dirs also

--- scripts/prep-prod-server.sh
+++ scripts/prep-prod-server.sh
@@ -9,12 +9,16 @@ mkdir -p .vercel/output
 echo "copying serverless functions to .vercel/output"
 cp -R srv/functions .vercel/output
 
+echo "copying config.json into .vercel/output"
+cp srv/config.json .vercel/output
+
 echo "copying dist directory into .vercel/output"
 cp -R dist .vercel/output
 
-echo "what do we have?"
+echo "what do we have in .vercel/output?"
 ls -al .vercel/output
 
+echo "------------------------------"
 
 echo "Move dist/ directory one level deep"
 mv dist static
@@ -24,3 +28,6 @@ mv dist/static dist/dist
 
 echo "copying functions into dist directory instead"
 cp -R srv/functions dist
+
+echo "copying config.json into dist direct"
+cp srv/config.json dist
