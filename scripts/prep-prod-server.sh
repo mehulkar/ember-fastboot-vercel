6269e22507bb864d0b161b5bacf5249f3d7de4f9
Mehul Kar <mehul.kar@vercel.com>
2022-08-05 17:42:51 -0700

    Attempt to setup a pre-render function that will call fastboot

---
+++ scripts/prep-prod-server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "deleting .vercel/output directory"
+rm -rf .vercel/output || true
+
+echo "creating .vercel/output directory"
+mkdir -p .vercel/output
+
+echo "copying serverless functions to .vercel/output"
+cp -R srv/functions .vercel/output
+
+echo "copying dist directory into .vercel/output"
+cp -R dist .vercel/output
+
+echo "what do we have?"
+ls .vercel/ouptut
---
+++ srv/functions/fastboot.func/index.js
@@ -0,0 +1,14 @@
+const FastBoot = require('fastboot');
+
+module.exports = async (req, res) => {
+  let app = new FastBoot({ distPath: './dist' });
+
+  console.log('--------------------');
+  console.log(`req.url`, req.url);
+  console.log(`req.path`, req.path);
+  console.log('--------------------');
+
+  const result = await app.visit(req.url);
+  const html = await result.html();
+  res.send(html);
+};
